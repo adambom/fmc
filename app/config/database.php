ef76bc2861865e413dff0d48086fdbf172b2ebec
Adam Savitzky <adam.savitzky@gmail.com>
2010-11-16 22:36:24 -0500

    added config file

---
+++ app/config/database.php
@@ -0,0 +1,21 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'fmc',
+	);
+	var $test = array(
+		'driver' => 'mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'fmc',
+	);
+}
+?>
