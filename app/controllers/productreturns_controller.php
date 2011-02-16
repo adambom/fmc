f6644db3dcea7d412fbbbe27cc0555c7083e6111
Adam Savitzky <adam.savitzky@gmail.com>
2011-02-16 06:02:31 -0500

    testing out more verbose part listing on pr views

--- app/controllers/productreturns_controller.php
+++ app/controllers/productreturns_controller.php
@@ -51,7 +51,7 @@ class ProductreturnsController extends AppController {
 		}
 		$vendors = $this->Productreturn->Vendor->find('list', array('order' => array('Vendor.name ASC')));
 		$jobs = $this->Productreturn->Job->find('list');
-		$parts = $this->Productreturn->Part->find('list', array(
+		$parts = $this->Productreturn->Part->find('all', array(
 			'order' => array('Part.partnumber DESC'),
 			'fields' => array('Part.id', 'Part.partnumber', 'Part.deviceid', 'Part.description')
 		));
